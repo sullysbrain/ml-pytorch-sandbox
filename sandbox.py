d07d831348ae98ab938df93c7488c7ae05d0e86b
Scott Sullivan <scott@sullysbrain.com>
2024-08-20 19:03:39 -0400

    Start prep for tensor work

--- sandbox.py
+++ sandbox.py
@@ -2,5 +2,12 @@ import torch
 
 torch.__version__
 
-torch.cuda.is_available()
+if (torch.cuda.is_available()):
+    print("CUDA is available")
+
+if (torch.backends.mps.is_available()):
+    print("M3 Macbook GPU:  MPS is available")
+else:
+    print("CUDA is not available")
+
 
